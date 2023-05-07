a6bf5433fef955470cabfd38b5a4eb58eb024250
walwizard <goneisgone@hotmail.com>
2023-05-07 13:57:53 -0300

    Big Burger - Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages/home/Home";
+
+
 
 const App = () => {
   return (
     <>
-    
+      <Home />    
     </>
   )
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+import background from "../../components/img/session-img-1.png"
+
+const Home = () => {
+    return (
+        <div className="home__page" style={{ backgroundImage: `url(${background})`}}>
+            <div className="infos">
+                <h1 className="title">Get Cashback <br /> up to 50%</h1>
+                <p className="sub__title">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur nesciunt reprehenderit, asperiores praesentium illo quasi rem libero.</p>
+                <h3 className="btn--orange">order now</h3>
+            </div>
+        </div >
+    )
+}
+
+export default Home;
