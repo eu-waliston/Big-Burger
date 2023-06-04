6f6bbe67105af0063e09e0ebfc9e01ec8b59515b
walwizard <goneisgone@hotmail.com>
2023-06-04 17:00:08 -0300

    BB - Best Burger - BP section

--- src/pages/BestBurgers/BestBurger.jsx
+++ src/pages/BestBurgers/BestBurger.jsx
@@ -4,8 +4,35 @@ import "./BestBurger.css";
 
 const BestBurger = () => {
 
-    return(
+    return (
         <div id="best-burger" className="bestburger--section">
+            <div className="best-burgers">
+
+                <div className="best-burger-1">
+                    <div className="left-content">
+                        <img src={require("../../components/img/burger-1.png")} alt="burger" />
+                    </div>
+                    <div className="right-content">
+                        <h1>Best Burger</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi accusantium velit rerum, ea pariatur sint ipsa,</p>
+                        <button>order now</button>
+                    </div>
+                </div>
+
+
+                <div className="best-burger-2">
+
+                    <div className="left-content">
+                        <img src={require("../../components/img/burger-2.png")} alt="burger" />
+                    </div>
+                    <div className="right-content">
+                        <h1>Best Burger</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi accusantium velit rerum, ea pariatur sint ipsa,</p>
+                        <button>order now</button>
+                    </div>
+
+                </div>
+            </div>
 
         </div>
     )
